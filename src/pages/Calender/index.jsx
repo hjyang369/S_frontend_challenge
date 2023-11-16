e38cf7617926b331f499b1e8923b92270a221928
hjyang369 <ghlwls6@gmail.com>
2023-11-16 23:28:39 +0900

    feat: 달력 기능 구현 중

--- src/pages/Calender/index.jsx
+++ src/pages/Calender/index.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import "./calender.css";
 
-export default function Calender() {
+const Calendar = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
-  const [openMemo, setOpenMemo] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [memoText, setMemoText] = useState("");
+  const [showModal, setShowModal] = useState(false);
+  const [memos, setMemos] = useState([]);
 
   const daysOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
 
@@ -37,8 +40,51 @@ export default function Calender() {
     setCurrentDate(newDate);
   };
 
-  const handleMemo = (id) => {
-    setOpenMemo((prev) => ({ ...prev, [id]: !prev[id] }));
+  const handleDateClick = (day) => {
+    setSelectedDate(day);
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    setSelectedDate(null);
+    setMemoText("");
+  };
+
+  const addMemo = () => {
+    if (memoText.trim() !== "") {
+      const dateKey = currentDate.toISOString().split("T")[0]; // 현재 날짜를 'YYYY-MM-DD' 형식으로 가져옴
+
+      // memos 배열에서 해당 날짜에 대한 메모 목록을 가져옴
+      const memoForDate = memos.find((item) => item.date === dateKey);
+
+      if (memoForDate) {
+        // 해당 날짜의 메모 목록이 이미 존재할 경우 추가
+        const updatedMemos = memos.map((item) =>
+          item.date === dateKey
+            ? { ...item, memos: [...item.memos, memoText] }
+            : item
+        );
+        setMemos(updatedMemos);
+      } else {
+        // 해당 날짜의 메모 목록이 없을 경우 새로 추가
+        setMemos([...memos, { date: dateKey, memos: [memoText] }]);
+      }
+
+      setMemoText("");
+    }
+  };
+  console.log(memos);
+
+  const deleteMemo = (dateKey, idx) => {
+    const updatedMemo = memos.filter((_, i) => i !== idx);
+    setMemos(updatedMemo);
+    const updatedMemos = memos.map((item) =>
+      item.date === dateKey
+        ? { ...item, memos: item.memos.filter((_, i) => i !== idx) }
+        : item
+    );
+    setMemos(updatedMemos);
   };
 
   const renderCalendar = () => {
@@ -67,20 +113,55 @@ export default function Calender() {
             <div key={`blank-${index}`} className="empty"></div>
           ))}
           {days.map((day) => (
-            <div onClick={() => handleMemo(day)} key={day} className="day">
+            <div key={day} className="day" onClick={() => handleDateClick(day)}>
               {day}
-              {openMemo[day] && (
-                <div className="memoContainer">
-                  <input type="text" placeholder="내용을 입력해주세요."></input>
-                  <button>등록</button>
-                </div>
-              )}
             </div>
           ))}
         </div>
+        {showModal && (
+          <div className="modal">
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
+            <h2>
+              {currentDate.getMonth() + 1}월 {selectedDate}일 메모
+            </h2>
+            <div className="memoContainer">
+              <textarea
+                className="textarea"
+                rows="4"
+                cols="30"
+                value={memoText}
+                onChange={(e) => setMemoText(e.target.value)}
+                placeholder="메모를 입력하세요..."
+              ></textarea>
+              <button onClick={addMemo}>저장</button>
+            </div>
+
+            {memos.map((item) =>
+              item.date === currentDate.toISOString().split("T")[0] ? (
+                <div key={item.date}>
+                  {item.memos.map((memo, idx) => (
+                    <div key={idx} className="createdMemo">
+                      <p>{memo}</p>
+                      <span
+                        className="close"
+                        onClick={() => deleteMemo(item.date, idx)}
+                      >
+                        &times;
+                      </span>
+                    </div>
+                  ))}
+                </div>
+              ) : null
+            )}
+          </div>
+        )}
       </div>
     );
   };
 
   return <div>{renderCalendar()}</div>;
-}
+};
+
+export default Calendar;
